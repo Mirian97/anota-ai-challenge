d486fde65d890d539998806c4db8acdb230638aa
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-11-25 21:35:09 -0300

    feat: create a message DTO

---
+++ src/main/java/com/quispe/anota_ai_challenge/services/aws/MessageDTO.java
@@ -0,0 +1,9 @@
+package com.quispe.anota_ai_challenge.services.aws;
+/*
+ * Quando precisa de classes simples que só armazenam dados.
+ * Para criar DTOs (como no seu exemplo).
+ * Sempre que quiser um código mais limpo e menos verboso.
+ */
+public record MessageDTO(String message) {
+    
+}
