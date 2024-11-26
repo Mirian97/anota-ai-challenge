a9b283db27cbe5e199cc23a05fb6c8cf6c540bd9
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-11-25 21:34:37 -0300

    feat: create custom exceptions

---
+++ src/main/java/com/quispe/anota_ai_challenge/domain/category/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,5 @@
+package com.quispe.anota_ai_challenge.domain.category.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException {
+    
+}
---
+++ src/main/java/com/quispe/anota_ai_challenge/domain/product/exceptions/ProductNotFoundException.java
@@ -0,0 +1,5 @@
+package com.quispe.anota_ai_challenge.domain.product.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    
+}
